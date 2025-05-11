bc04f7c6c354ce8a463ab925db4525b2f5164122
Tai Le <161448595+blackicecoffee@users.noreply.github.com>
2025-05-11 14:09:48 +0700

    🔨 Update template for prompt techniques

--- prompt_techniques/base_prompt.py
+++ prompt_techniques/base_prompt.py
@@ -1,13 +1,15 @@
 from langchain_core.messages import BaseMessage
 
+from typing import List
+
 from models.base_model import BaseLLMModel
 
 class BasePrompt:
     def __init__(self, prompt_instruction_path: str):
         self.prompt_instruction_path = prompt_instruction_path
 
-    def generate_prompt(self, *args):
+    def generate_prompt(self, *args) -> List[BaseMessage]:
         pass
 
-    def generate_response(self, model: BaseLLMModel, messages: BaseMessage):
+    def generate_response(self, model: BaseLLMModel, messages: List[BaseMessage]):
         pass

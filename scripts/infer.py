d023ffb7c2858c0656fb263c4dda300775c4e33e
Tai Le <161448595+blackicecoffee@users.noreply.github.com>
2025-05-16 02:59:14 +0700

    🔨 Add scripts for inference using vanilla prompting

--- scripts/infer.py
+++ scripts/infer.py
@@ -1,7 +1,14 @@
 import argparse
+import json
+from dotenv import load_dotenv
 
 from models.llama_vision import LlamaVision
+from prompt_techniques.vanilla_prompt.vanilla_prompt import VanillaPrompt
+from helpers.image_to_url import image_to_data_url
+from helpers.ground_truth_reader import read_fields_from_ground_truth
 from metrics import f1_score, exact_match, similarity_score
+
+load_dotenv()
 
 # Create arguments parser
 parser = argparse.ArgumentParser()
@@ -19,10 +26,27 @@ if args.model == "llama_vision":
     model = LlamaVision()
 
 if args.image_path:
-    pass
+    image_path = args.image_path
 
-if args.prompt_technique:
-    pass
+if args.prompt_technique == "vanilla":
+    prompt_instruction_path = "prompt_instructions/vanilla/vanilla_instruction_v1.txt"
 
 if args.extract_table:
     pass
+
+async def predict():
+    image_data = image_to_data_url(image_path=image_path)
+    fields = read_fields_from_ground_truth(image_path=image_path)
+
+    response = await VanillaPrompt(
+                prompt_instruction_path=prompt_instruction_path
+            ).generate_response(model=model, fields=fields, image_data=image_data)
+
+    results = json.loads(response)
+
+    print("Result:\n", json.dumps(results, indent=4))
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(predict())

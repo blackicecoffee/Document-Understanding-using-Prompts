ecb72761393ecab92ab430c0a583196fdcd9f412
Tai Le <161448595+blackicecoffee@users.noreply.github.com>
2025-05-20 18:48:11 +0700

    🔨 Update metrics for calculating table results

--- helpers/metrics.py
+++ helpers/metrics.py
@@ -21,11 +21,31 @@ def exact_match(ground_truth: dict, pred: dict):
             continue
         if k != "Table":
             num_field += 1
-            gt_value = ground_truth[k].strip().replace(" ", "").lower()
-            pred_value = pred[k].strip().replace(" ", "").lower()
+            gt_value = " ".join(ground_truth[k].lower().split())
+            pred_value = " ".join(pred[k].lower().split())
 
             if gt_value == pred_value:
                 em_score += 1
+
+        elif k == "Table":
+            gt_table = ground_truth[k]
+            pred_table = pred[k]
+            
+            num_field += len(gt_table)
+
+            for idx in range(len(pred_table)):
+                if idx > len(gt_table): break
+
+                gt_row = gt_table[idx]
+                pred_row = pred_table[idx]
+
+                for col_name in gt_row.keys():
+                    if col_name not in pred_row: continue
+                    gt_col_value = " ".join(gt_row[col_name].lower().split())
+                    pred_col_value = " ".join(pred_row[col_name].lower().split())
+
+                    if gt_col_value == pred_col_value:
+                        em_score += 1
 
     return round(float(em_score) / float(num_field) * 100.0, 4)
 
@@ -40,8 +60,8 @@ def similarity_score(ground_truth: dict, pred: dict):
             continue
         if k != "Table":
             num_field += 1
-            gt_value = ground_truth[k].strip().replace(" ", "").lower()
-            pred_value = pred[k].strip().replace(" ", "").lower()
+            gt_value = " ".join(ground_truth[k].lower().split())
+            pred_value = " ".join(pred[k].lower().split())
 
             vectorizer = TfidfVectorizer()
             tfidf_matrix = vectorizer.fit_transform([gt_value, pred_value])
@@ -49,6 +69,30 @@ def similarity_score(ground_truth: dict, pred: dict):
             similarity = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])[0][0]
 
             total_similarty += similarity
+
+        elif k == "Table":
+            gt_table = ground_truth[k]
+            pred_table = pred[k]
+            
+            num_field += len(gt_table)
+
+            for idx in range(len(pred_table)):
+                if idx > len(gt_table): break
+
+                gt_row = gt_table[idx]
+                pred_row = pred_table[idx]
+
+                for col_name in gt_row.keys():
+                    if col_name not in pred_row: continue
+                    gt_col_value = " ".join(gt_row[col_name].lower().split())
+                    pred_col_value = " ".join(pred_row[col_name].lower().split())
+
+                    vectorizer = TfidfVectorizer()
+                    tfidf_matrix = vectorizer.fit_transform([gt_col_value, pred_col_value])
+                    
+                    similarity = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])[0][0]
+
+                    total_similarty += similarity
     
     return round(float(total_similarty) / float(num_field) * 100.0, 4)
 
@@ -64,8 +108,8 @@ def get_all_scores(ground_truth: dict, pred: dict):
             continue
         if k != "Table":
             num_field += 1
-            gt_value = ground_truth[k].strip().replace(" ", "").lower()
-            pred_value = pred[k].strip().replace(" ", "").lower()
+            gt_value = " ".join(ground_truth[k].lower().split())
+            pred_value = " ".join(pred[k].lower().split())
 
             # Get exact match scores
             if gt_value == pred_value:
@@ -79,4 +123,33 @@ def get_all_scores(ground_truth: dict, pred: dict):
 
             total_similarty += similarity
 
+        elif k == "Table":
+            gt_table = ground_truth[k]
+            pred_table = pred[k]
+            
+            num_field += len(gt_table)
+
+            for idx in range(len(pred_table)):
+                if idx > len(gt_table): break
+
+                gt_row = gt_table[idx]
+                pred_row = pred_table[idx]
+
+                for col_name in gt_row.keys():
+                    if col_name not in pred_row: continue
+                    gt_col_value = " ".join(gt_row[col_name].lower().split())
+                    pred_col_value = " ".join(pred_row[col_name].lower().split())
+
+                    # Get exact match scores
+                    if gt_col_value == pred_col_value:
+                        em_score += 1
+
+                    # Get similarity scores
+                    vectorizer = TfidfVectorizer()
+                    tfidf_matrix = vectorizer.fit_transform([gt_col_value, pred_col_value])
+                    
+                    similarity = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])[0][0]
+
+                    total_similarty += similarity
+                    
     return {"EM": round(float(em_score) / float(num_field) * 100.0, 4), "similarity_score": round(float(total_similarty) / float(num_field) * 100.0, 4)}

a990d16d3431f512d64c2d0d2afbd911b77755ed
Tai Le <161448595+blackicecoffee@users.noreply.github.com>
2025-05-21 16:05:34 +0700

    🔨 Update template for different prompting techniques: split extract information and extract table information

--- prompt_techniques/base_prompt.py
+++ prompt_techniques/base_prompt.py
@@ -17,6 +17,14 @@ class BasePrompt:
     def generate_table_prompt(self, *args) -> List[BaseMessage]:
         """Generate prompt to extract information from table"""
         pass
+    
+    def extract_information(self, model: BaseLLMModel, messages: List[BaseMessage]):
+        """Extract information not from table"""
+        pass
+    
+    def extract_table_information(self, model: BaseLLMModel, messages: List[BaseMessage]):
+        """Extract information from table"""
+        pass
 
     def generate_response(self, model: BaseLLMModel, messages: List[BaseMessage]):
         """Generate response"""

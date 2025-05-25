60b6bb3bb3338ed868a31fea834591fec9f0f5d3
Tai Le <161448595+blackicecoffee@users.noreply.github.com>
2025-05-25 23:10:44 +0700

    🔨 Update scripts for model evaluation using self-consistency prompting

--- scripts/infer.py
+++ scripts/infer.py
@@ -7,6 +7,7 @@ from models.llama_vision import LlamaVision
 from models.base_model import BaseLLMModel
 from prompt_techniques.vanilla_prompt.vanilla_prompt import VanillaPrompt
 from prompt_techniques.Few_Shots.few_shot_prompt import FewShotsPrompt
+from prompt_techniques.self_consistency.self_consistency_prompt import SelfConsistencyPrompt
 from helpers.image_to_url import image_to_data_url
 from helpers.ground_truth_reader import read_ground_truth, read_fields_from_ground_truth, read_table_column_from_ground_truth
 from helpers.get_examples import get_random_examples
@@ -20,7 +21,9 @@ async def predict(
         prompt_instruction_path: str, 
         table_instruction_path: str, 
         image_path: str,
-        num_samples: int
+        num_samples: int,
+        base_prompt: str,
+        retries: int
     ):
     await asyncio.sleep(0)
 
@@ -44,6 +47,17 @@ async def predict(
             examples=examples
         ).generate_response(model=model, fields=fields, table_columns=table_columns, image_data=image_data)
 
+    elif prompt_technique == "self_consistency":
+        examples = get_random_examples(image_path=image_path, num_samples=num_samples)
+        
+        results = await SelfConsistencyPrompt(
+            prompt_instruction_path=prompt_instruction_path,
+            table_instruction_path=table_instruction_path,
+            baseline_prompt=base_prompt,
+            examples=examples,
+            k=retries
+        ).generate_response(model=model, fields=fields, table_columns=table_columns, image_data=image_data)
+
     scores = get_all_scores(ground_truth=ground_truth, pred=results)
 
     return [results, scores]
@@ -56,8 +70,10 @@ if __name__ == "__main__":
     parser.add_argument("--model", help="Name of the LLM", type=str, default="llama_vision")
     parser.add_argument("--image_path", help="Path to the image", type=str)
     parser.add_argument("--prompt_technique", help="Prompting technique", type=str, default="vanilla")
+    parser.add_argument("--base_prompt", help="Base prompting technique for self-consistency prompt", type=str, default="vanilla")
     parser.add_argument("--extract_table", help="Option to extract table", type=str, default="False")
     parser.add_argument("--num_samples", help="Number of samples using for few shots prompting", type=int, default=1)
+    parser.add_argument("--retries", help="Number of retries for self-consistency prompting", type=int, default=3)
 
     # Parse the arguments
     args = parser.parse_args()
@@ -83,6 +99,24 @@ if __name__ == "__main__":
         if args.extract_table == "True":
             table_instruction_path = "prompt_instructions/Few_Shots/fewshots_instruction_v2.txt"
 
+    elif args.prompt_technique == "self_consistency":
+        if args.base_prompt == "vanilla":
+            prompt_instruction_path = "prompt_instructions/vanilla/vanilla_instruction_v1.txt"
+            table_instruction_path = None
+        
+            if args.extract_table == "True":
+                table_instruction_path = "prompt_instructions/vanilla/vanilla_instruction_v2.txt"
+        
+        elif args.base_prompt == "few_shots":
+            prompt_instruction_path = "prompt_instructions/Few_Shots/fewshots_instruction_v1.txt"
+            table_instruction_path = None
+            
+            if args.extract_table == "True":
+                table_instruction_path = "prompt_instructions/Few_Shots/fewshots_instruction_v2.txt"
+        
+        else:
+            raise ValueError("Not found baseline prompt for self-consistency prompting!")
+
     results, scores = asyncio.run(
         predict(
             model=model,
@@ -90,11 +124,18 @@ if __name__ == "__main__":
             prompt_instruction_path=prompt_instruction_path, 
             table_instruction_path=table_instruction_path,
             image_path=image_path,
-            num_samples=args.num_samples
+            num_samples=args.num_samples,
+            base_prompt=args.base_prompt,
+            retries=args.retries
         )
     )
 
     print(f"Prompting technique: {args.prompt_technique}")
+
+    if args.prompt_technique == "self_consistency":
+        print(f"Base prompt: {args.base_prompt}")
+        print(f"Retries: {args.retries}")
+
     print(f"Extract table: {args.extract_table}")
     print(f"# samples: {args.num_samples}\n")
     print("Result:\n", json.dumps(results, indent=4))

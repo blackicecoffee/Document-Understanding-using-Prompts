264632cda37415546e48288971c3bf73a2a9ccbd
Tai Le <161448595+blackicecoffee@users.noreply.github.com>
2025-06-06 00:17:00 +0700

    🔨 Add metrics for similarity using sentence transformers

--- helpers/metrics/similarity.py
+++ helpers/metrics/similarity.py
@@ -2,6 +2,11 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+from sentence_transformers import SentenceTransformer
+
+print("Initializing Sentence Transformers...")
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
 def tf_idf(str1: str, str2: str):
     vectorizer = TfidfVectorizer()
     tfidf_matrix = vectorizer.fit_transform([str1, str2])
@@ -10,6 +15,12 @@ def tf_idf(str1: str, str2: str):
 
     return similarity
 
-def similarity_sentence_transformers():
-    """Calculate similarity score using Sentence Transformers"""
-    pass
+def similarity_sentence_transformers(str1: str, str2: str):
+    """Calculate similarity score using Sentence Transformers"""    
+    sentences = [str1, str2]
+
+    embeddings = model.encode(sentences)
+
+    similarity = model.similarity(embeddings, embeddings).numpy()[0, 1]
+
+    return similarity
